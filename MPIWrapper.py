a24634e616743aa9a90e6fe8af1f26ae0dedf42e
Nicholai Goodall <Kranoscorp@gmail.com>
2014-06-02 12:32:40 -0400

    Added wrappers for both weight functions
    Functionality not tested

--- MPIWrapper.py
+++ MPIWrapper.py
@@ -10,10 +10,20 @@ class wrap:
   def getNumWeights(self, f):
     def newf(self):
       if(rank != 0):
-        MPI.send(
+        data = f()
+        MPI.send(data, dest=0, tag=23)
+      else
+        comm.Reduce(None, data, op=MPI.SUM, root=0)
+        return data
   
   def getWeights(self, f):
-    
+    def newf(self):
+      if(rank != 0):
+        data = f()
+        MPI.send(data, dest=0, tag=23)
+      else
+        comm.Gatherv(data)
+        return data
   
   def simulateNet(self, f):
     
